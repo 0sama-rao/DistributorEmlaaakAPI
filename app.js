48417919797c802767d38aa3d69fca6e43fce42b
Muhammad Osama <94981090+0sama-rao@users.noreply.github.com>
2023-01-27 17:16:19 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var  db = require('./controllers/userController');
+
+// const jwt = require('jasonwebtoken')
 var  express = require('express');
 var  bodyParser = require('body-parser');
 var  cors = require('cors');
@@ -11,12 +13,13 @@ var  app = express();
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 
-var  router = express.Router();
+  var  router = express.Router();
 
 app.use(bodyParser.urlencoded({ extended:  true }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/api', router);
+
 
 router.use((request, response, next) => {
   console.log('Method Executed !');
@@ -24,13 +27,14 @@ router.use((request, response, next) => {
 });
 
 
+
 //to list all the users/investors
-router.route('/users').get((request, response) => {
-    
-    db.getUsers().then((data) => {
-      response.json(data[0]);
+  router.route('/users').get((request, response) => {
+      
+      db.getUsers().then((data) => {
+        response.json(data[0]);
+      })
     })
-  })
   
 //getDaccountSSA
 router.route('/accountSSA').get((request, response) => {
@@ -69,7 +73,26 @@ router.route('/addUser').post((request, response) => {
       
     })
   
+    //Create account callback
+    router.route('/createAccountCallback').post((request, response) => {
+      let user = {...request.body}
+    
+      db.createAccountCallback(user).then(function(result) { 
+          // console.log(response.status)  
 
+          response.status (200).json({result})
+          // if(result == true){
+          //   response.status(200).json({result})
+          //   console.log(result)
+          // }
+          //   else{
+          //   response.status(200).json({result})
+          //  console.log(result)
+          // }
+          
+        })
+      })
+    
 
 
 router.route('/createAccountSSA').post((request, response) => {
@@ -82,11 +105,13 @@ router.route('/createAccountSSA').post((request, response) => {
     }
       else{
       response.status(200).json({result, message:"Error Inserting record or Duplicate account data!"})
-        // console.log(response.status)
+      console.log(result)  
+      //  console.log(result.recordset.length)
     }
     
   })
 })
+//updateRiskProfiling
 
 router.route('/createAccountRegular').post((request, response) => {
   let user = {...request.body}
@@ -101,8 +126,29 @@ router.route('/createAccountRegular').post((request, response) => {
       
       else{
       response.status(400).json({result,RequestError, message:"Error Inserting record or Duplicate account data!"})
-        console.log(user.emlaakTransactionId.length)  
-      // console.log(user)
+      console.log(user)  
+       //console.log(user)
+    }
+    
+  })
+})
+
+//Update Risk profiling 
+
+router.route('/updateRiskProfiling').post((request, response) => {
+  let user = {...request.body}
+  
+   db.updateRiskProfiling(user).then(function(result, RequestError) {
+
+  //   response.status(200).json(err)
+    if(result == true){
+      response.status(200).json({result, message:"Record Inserted"})
+    }
+      
+      else{
+      response.status(400).json({result,RequestError, message:"Error Inserting record or Duplicate account data!"})
+      console.log(user)  
+       //console.log(user)
     }
     
   })
