e9e7f04e71d262a3eb6559fd691e6f9b535cc6df
Muhammad Osama <94981090+0sama-rao@users.noreply.github.com>
2023-03-07 14:52:12 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var  db = require('./controllers/userController');
 var  db1 = require('./controllers/investment');
+var  db2 = require('./controllers/YpayController');
 
 // const jwt = require('jasonwebtoken')
 var  express = require('express');
@@ -13,10 +14,11 @@ var  app = express();
 
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
-
+  
   var  router = express.Router();
 
-app.use(bodyParser.urlencoded({ extended:  true }));
+
+  app.use(bodyParser.urlencoded({ extended:  true }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/api', router);
@@ -182,6 +184,28 @@ router.route('/updateRiskProfiling').post((request, response) => {
     
   })
 })
+
+
+router.route('/ypayAccount').post((request, response) => {
+  let user = {...request.body}
+  
+   db2.newAccountPost(user).then(function(result, RequestError) {
+
+  //   response.status(200).json(err)
+    if(result == true){
+      response.status(200).json({result, message:"Record Inserted"})
+    }
+      
+      else{
+      response.status(400).json({result,RequestError, message:"Error Inserting record or Duplicate account data!"})
+      // console.log(user)  
+       //console.log(user)
+    }
+    
+  })
+})
+
+
 
 
 //Create Investment
